db2b32aa62de9eaefc6d246d3bc651593990cc36
BANDARU NARASIMHAGUPTA <115392495+Narasimhagupta2004@users.noreply.github.com>
2025-04-23 11:14:49 +0530

    Update LoginAttempt.js

--- models/LoginAttempt.js
+++ models/LoginAttempt.js
@@ -1,27 +1,3 @@
-// const mongoose = require('mongoose');
-
-// const LoginAttemptSchema = new mongoose.Schema({
-//     email: {
-//         type: String,
-//         required: true,
-//     },
-//     success: {
-//         type: Boolean,
-//         required: true,
-//     },
-//     timestamp: {
-//         type: Date,
-//         default: Date.now,
-//     },
-//     ipAddress: {
-//         type: String,
-//         required: false,
-//     }
-// });
-
-// module.exports = mongoose.model('LoginAttempt', LoginAttemptSchema);
-// models/LoginAttempt.js
-// models/LoginAttempt.js
 const mongoose = require('mongoose');
 
 const LoginAttemptSchema = new mongoose.Schema({
