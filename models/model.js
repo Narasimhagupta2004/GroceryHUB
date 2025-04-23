9bb28e9a57dc826001dd89939b47f8d6860287f1
BANDARU NARASIMHAGUPTA <115392495+Narasimhagupta2004@users.noreply.github.com>
2025-04-23 11:15:05 +0530

    Update model.js

--- models/model.js
+++ models/model.js
@@ -1,27 +1,3 @@
-// const mongoose = require('mongoose');
-// let Registeruser = new mongoose.Schema({
-//     username:{
-//         type : String,
-//         required : true,
-//     },
-//     email:{
-//     type:String,
-//     required:true,
-//     unique : true,
-//     },
-//     password:{
-//         type:String,
-//         required:true,
-//     },
-//     confirmpassword:{
-//         type:String,
-//         required : true,
-//     }
-// })
-
-// module.exports = mongoose.model('Registeruser',Registeruser);
-// models/model.js
-// models/model.js
 const mongoose = require('mongoose');
 
 const RegisteruserSchema = new mongoose.Schema({
